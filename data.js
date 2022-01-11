8f04cc8f373e837649354b133b3124adba8151fc
JulianCampolongo <92273387+JulianCampolongo@users.noreply.github.com>
2022-01-11 17:17:14 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+        name: 'La Niñera',
+        des: 'Hola, soy Julian Campolongo y estoy realizando una web de series y pelis de los 90! Ojala les guste! Me ayudan muchisimo con una reaccion o un comment! Muchas gracias :):):)',
+        image: '/fine2.jpeg'
+    },
+    {
+        name: ' Padre De Familia',
+        des: 'Hola, soy Julian Campolongo y estoy realizando una web de series y pelis de los 90! Ojala les guste! Me ayudan muchisimo con una reaccion o un comment! Muchas gracias :):):)',
+        image: '/drepa.jpg'
+    },
+    {
+        name: 'Dragon Ball Z',
+        des: 'Hola, soy Julian Campolongo y estoy realizando una web de series y pelis de los 90! Ojala les guste! Me ayudan muchisimo con una reaccion o un comment! Muchas gracias :):):)',
+        image: '/dbz1.jpg'
+    },
+    {
+        name: 'Slam Dunk',
+        des: 'Hola, soy Julian Campolongo y estoy realizando una web de series y pelis de los 90! Ojala les guste! Me ayudan muchisimo con una reaccion o un comment! Muchas gracias :):):)',
+        image: '/SLAM.jpg'
+    },
+    {
+        name: 'One Piece',
+        des: 'Hola, soy Julian Campolongo y estoy realizando una web de series y pelis de los 90! Ojala les guste! Me ayudan muchisimo con una reaccion o un comment! Muchas gracias :):):)',
+        image: '/one.jpg'
+    }
+]
